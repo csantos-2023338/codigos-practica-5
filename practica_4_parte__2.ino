e45795b3a48c12692b99204c8ad0fd79102957e7
csantos-2023338 <168783737+csantos-2023338@users.noreply.github.com>
2024-05-10 16:24:48 -0600

    Update practica_4_parte__2.ino

--- practica_4_parte__2.ino
+++ practica_4_parte__2.ino
@@ -1,161 +1,70 @@
-
- /*
+/*
    Fundacion Kinal
-   Centro educativo tecnico laboral Kinal
+   Centro educativo tecnico laboral kinal
    Quinto perito
    Quinto electronica
-   Codigo Tecnico: EB4AM 
+   Codigo Tecnico: EB5AV
    Curso: Taller de electronica digital y reparacion de computadoras I
-   Proyecto: Marcha imperial
-   Dev: Hugo Alexander Reyes Veliz -2023132
-   Fecha: 2 de mayo
+   Proyecto: Catálogo de Productos.
+   Dev: Carlos Santos
+   Fecha: 28 de abril
 */
-
-const int c = 261;
-const int d = 294;
-const int e = 329;
-const int f = 349;
-const int g = 391;
-const int gS = 415;
-const int a = 440;
-const int aS = 455;
-const int b = 466;
-const int cH = 523;
-const int cSH = 554;
-const int dH = 587;
-const int dSH = 622;
-const int eH = 659;
-const int fH = 698;
-const int fSH = 740;
-const int gH = 784;
-const int gSH = 830;
-const int aH = 880;
-
-const int buzzerPin = 9; 
-const int ledPin1 = 12;  
-const int ledPin2 = 13;  
-
-int counter = 0;
-
+int D2 =(2);
+int D3 =(3);
+int D4 =(4);
+int D5 =(5);
+int INTERRUPTOR1 = (8);
+int Seleccionar = 1;
+ 
 void setup()
 {
-  
-  pinMode(buzzerPin, OUTPUT); 
-  pinMode(ledPin1, OUTPUT);  
-  pinMode(ledPin2, OUTPUT);
+pinMode (2, OUTPUT);
+pinMode (3, OUTPUT);
+pinMode (4, OUTPUT);
+pinMode (5, OUTPUT);
+pinMode (8, INPUT);
+pinMode (9, INPUT);
+pinMode (10, INPUT);
 }
-
+ 
 void loop()
 {
-
- 
-  firstSection();
-
-
-  secondSection();
-
-    
-  beep(f, 250);  
-  beep(gS, 500);  
-  beep(f, 350);  
-  beep(a, 125);
-  beep(cH, 500);
-  beep(a, 375);  
-  beep(cH, 125);
-  beep(eH, 650);
-
-  delay(500);
-
- 
-  secondSection();
-
-  beep(f, 250);  
-  beep(gS, 500);  
-  beep(f, 375);  
-  beep(cH, 125);
-  beep(a, 500);  
-  beep(f, 375);  
-  beep(cH, 125);
-  beep(a, 650);  
-
-  delay(650);
-}
-
-void beep(int note, int duration)
-{
- 
-  tone(buzzerPin, note, duration);
-
- 
-  if(counter % 2 == 0)
-  {
-    digitalWrite(ledPin1, HIGH);
-    delay(duration);
-    digitalWrite(ledPin1, LOW);
-  }else
-  {
-    digitalWrite(ledPin2, HIGH);
-    delay(duration);
-    digitalWrite(ledPin2, LOW);
-  }
-
   
-  noTone(buzzerPin);
-
-  delay(50);
-
-
-  counter++;
-}
-
-void firstSection()
-{
-  beep(a, 500);
-  beep(a, 500);    
-  beep(a, 500);
-  beep(f, 350);
-  beep(cH, 150);  
-  beep(a, 500);
-  beep(f, 350);
-  beep(cH, 150);
-  beep(a, 650);
-
-  delay(500);
-
-  beep(eH, 500);
-  beep(eH, 500);
-  beep(eH, 500);  
-  beep(fH, 350);
-  beep(cH, 150);
-  beep(gS, 500);
-  beep(f, 350);
-  beep(cH, 150);
-  beep(a, 650);
-
-  delay(500);
-}
-
-void secondSection()
-{
-  beep(aH, 500);
-  beep(a, 300);
-  beep(a, 150);
-  beep(aH, 500);
-  beep(gSH, 325);
-  beep(gH, 175);
-  beep(fSH, 125);
-  beep(fH, 125);    
-  beep(fSH, 250);
-
-  delay(325);
-
-  beep(aS, 250);
-  beep(dSH, 500);
-  beep(dH, 325);  
-  beep(cSH, 175);  
-  beep(cH, 125);  
-  beep(b, 125);  
-  beep(cH, 250);  
-
-  delay(350);
+    if(digitalRead(INTERRUPTOR1) == HIGH){
+digitalWrite (D2, LOW);
+digitalWrite (D3, LOW);
+digitalWrite (D4, LOW);
+digitalWrite (D5, LOW);
+delay(1000);
+digitalWrite (D2, LOW);
+digitalWrite (D3, LOW);
+digitalWrite (D4, LOW);
+digitalWrite (D5, LOW);
+  delay(1000);
+digitalWrite (D2, HIGH);
+digitalWrite (D3, LOW);
+digitalWrite (D4, LOW);
+digitalWrite (D5, LOW);
+ delay(1000);
+digitalWrite (D2, HIGH);
+digitalWrite (D3, HIGH);
+digitalWrite (D4, LOW);
+digitalWrite (D5, LOW);
+   delay(1000);
+digitalWrite (D2, HIGH);
+digitalWrite (D3, LOW);
+digitalWrite (D4, HIGH);
+digitalWrite (D5, LOW);
+   delay(1000);
+digitalWrite (D2, HIGH);                                                                                      
+digitalWrite (D3, HIGH);
+digitalWrite (D4, HIGH);
+digitalWrite (D5, LOW);
+   delay(1000);
+digitalWrite (D2, HIGH);
+digitalWrite (D3, LOW);
+digitalWrite (D4, LOW);
+digitalWrite (D5, HIGH);
+    delay(1000);
+  }
 }
